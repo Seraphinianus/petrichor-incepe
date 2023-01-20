7fc37d5390ba78167d664ff85d105084361db170
Seraphinianus <73338681+Seraphinianus@users.noreply.github.com>
2023-01-20 22:13:56 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   Experience/Camera.js
    	new file:   Experience/Experience.js
    	new file:   Experience/Preloader.js
    	new file:   Experience/Renderer.js
    	new file:   Experience/Theme.js
    	new file:   Experience/Utils/Resources.js
    	new file:   Experience/Utils/Sizes.js
    	new file:   Experience/Utils/Time.js
    	new file:   Experience/Utils/assets.js
    	new file:   Experience/Utils/covertDivsToSpans.js
    	new file:   Experience/World/Controls.js
    	new file:   Experience/World/Environment.js
    	new file:   Experience/World/Floor.js
    	new file:   Experience/World/Room.js
    	new file:   Experience/World/World.js
    	new file:   counter.js
    	new file:   index.html
    	new file:   javascript.svg
    	new file:   main.js
    	new file:   netlify.toml
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/draco/README.md
    	new file:   public/draco/draco_decoder.js
    	new file:   public/draco/draco_decoder.wasm
    	new file:   public/draco/draco_encoder.js
    	new file:   public/draco/draco_wasm_wrapper.js
    	new file:   public/draco/gltf/draco_decoder.js
    	new file:   public/draco/gltf/draco_decoder.wasm
    	new file:   public/draco/gltf/draco_encoder.js
    	new file:   public/draco/gltf/draco_wasm_wrapper.js
    	new file:   public/models/Finale Version 16.glb
    	new file:   public/models/bucegi_scaled.glb
    	new file:   public/textures/PetrichorclipDemoFinalmp4.mp4
    	new file:   public/textures/videoplayback.mp4
    	new file:   public/vite.svg
    	new file:   style.css

---
+++ Experience/Utils/Time.js
@@ -0,0 +1,23 @@
+import { EventEmitter } from "events";
+
+export default class Time extends EventEmitter {
+    constructor() {
+        super(); // <- This gives us access to the EventEmitter
+        this.start = Date.now();
+        this.current = this.start;
+        this.elapsed = 0;
+        this.delta = 16;
+
+        this.update();
+    }
+
+    update() {
+        const currentTime = Date.now();
+        this.delta = currentTime - this.current;
+        this.current = currentTime;
+        this.elapsed = this.current - this.start;
+
+        this.emit("update");
+        window.requestAnimationFrame(() => this.update());
+    }
+}
